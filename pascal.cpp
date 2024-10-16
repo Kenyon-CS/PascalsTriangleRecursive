f1ea66a3ec329faf96011dcf39b6dfc84012b29a
James Skön <skonjp@kenyon.edu>
2024-10-16 08:00:11 -0400

    Add Memoization

--- pascal.cpp
+++ pascal.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <utility>
 
 using namespace std;
+
+map<pair<int,int>,int> mem;
 
 // Recursive function to calculate binomial coefficients
 int binomialCoefficient(int n, int k) {
@@ -10,7 +14,14 @@ int binomialCoefficient(int n, int k) {
         return 1;
     }
     // Recursive relation: C(n, k) = C(n-1, k-1) + C(n-1, k)
-    return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    pair<int,int> p = pair<int,int>(n,k);
+    if (mem.find(p) != mem.end()) {
+        return mem[p];
+    }
+
+    int s = binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    mem[p] = s;
+    return s;
 }
 
 // Function to generate Pascal's Triangle using recursion
